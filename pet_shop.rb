e5b06c3a1fda190fcce3c1bb76620bfb350cda65
Ariane Holford <aribomop@gmail.com>
2020-02-21 16:19:11 +0000

    added function 5 for increasing number of pets sold

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,27 +1,29 @@
 require('pry-byebug')
 
-#find shop name
+# 1- find shop name
 def pet_shop_name(shop)
-  return shop[:name]
+  shop[:name]
 end
 
-#add up total cash
+# 2 - add up total cash
 def total_cash(till)
-  return till[:admin][:total_cash]
+  till[:admin][:total_cash]
 end
 
-#add or remove cash
-
+#3 - add or remove cash
 def add_or_remove_cash(shop, amount)
   shop[:admin][:total_cash] += amount
 end
 
-# find number of pets sold
-
+# 4 - find number of pets sold
 def pets_sold(shop)
-  return shop[:admin][:pets_sold]
+  shop[:admin][:pets_sold]
 end
 
+# 5 - increase pets sold
+def increase_pets_sold(shop, amount)
+  shop[:admin][:pets_sold] += amount
+end
 =begin
 
 =end
