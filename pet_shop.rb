44b0c22873a52cfc1a586dfbd84dabb2716acafd
Ariane Holford <aribomop@gmail.com>
2020-02-21 16:15:55 +0000

    added functions 3-add or remove cash and 4-find number of pets sold

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,11 +1,27 @@
-#find shop name
+require('pry-byebug')
 
+#find shop name
 def pet_shop_name(shop)
   return shop[:name]
 end
 
 #add up total cash
-
 def total_cash(till)
   return till[:admin][:total_cash]
 end
+
+#add or remove cash
+
+def add_or_remove_cash(shop, amount)
+  shop[:admin][:total_cash] += amount
+end
+
+# find number of pets sold
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
+end
+
+=begin
+
+=end
