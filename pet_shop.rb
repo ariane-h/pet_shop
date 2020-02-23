a04c3202073b522854af85de985a7cd8873600a8
Ariane Holford <aribomop@gmail.com>
2020-02-23 16:24:06 +0000

    refactored find by pet name

--- pet_shop.rb
+++ pet_shop.rb
@@ -30,22 +30,27 @@ def stock_count(shop)
   shop[:pets].count
 end
 
-# 7 - number of pets by breed
+# 7 - type one, number of pets by breed
 # def pets_by_breed(shop, breed)
 #   all_pet_breeds = []
 #   shop[:pets].map { |pet| all_pet_breeds.push(pet[:breed]) }
 #   all_pet_breeds.select {|type| type == breed }
 # end
 
-#7 - number of pets by breed refactored
+#7 - type two, number of pets by breed refactored
 def pets_by_breed(shop, breed)
   shop[:pets].map.select {|pet| (pet[:breed] == breed)}
 end
 
-# 8 - find by pet name
+# 8 - type one, find by pet name
+# def find_pet_by_name(shop, name)
+#   all_pets = shop[:pets]
+#   all_pets.find {|pet| pet[:name] == name }
+# end
+
+# 8 - type two, find by pet name refactored
 def find_pet_by_name(shop, name)
-  all_pets = shop[:pets]
-  all_pets.find {|pet| pet[:name] == name }
+  shop[:pets].find {|pet| pet[:name] == name}
 end
 
 # 9 - remove pet by names
